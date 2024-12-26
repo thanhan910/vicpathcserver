ab2b99483be206965e4b7000fe986022dcaa328e
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-12-26 03:22:17 +0000

    Remove unused geometry classes and related implementations

--- geoobjects/point.cpp
+++
@@ -1,8 +0,0 @@
-#include "point.h"
-
-#include <cmath>
-
-double Point::distanceTo(const Point &other) const
-{
-    return std::sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
-}
--- geoobjects/point.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Point structure
-struct Point
-{
-    double x, y;
-
-    double distanceTo(const Point &other) const;
-};
