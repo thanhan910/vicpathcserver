992fcf3b9cc15f342d59afead74ea86b605fa090
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-12-31 03:11:31 +1100

    Debug

--- ptv/ptv.cpp
+++ ptv/ptv.cpp
@@ -14,19 +14,19 @@ struct pt_stop_time {
     pt_stop_id stop_id;
     int stop_sequence;
     int next_stop_sequence;
-}
+};
 
 struct pt_stop_time_key {
     std::string trip_id;
     int stop_sequence;
-}
+};
 
 struct pt_stop {
     std::string stop_name;
     double stop_lat;
     double stop_lon;
     std::vector<pt_stop_time_key> stop_times;
-}
+};
 
 using map_pt_stop_times = std::map<pt_trip_id, std::map<int, pt_stop_time>>;
 using map_pt_stops = std::map<pt_stop_id, pt_stop>;
@@ -35,7 +35,7 @@ using rd_point_ufi = int;
 
 struct rd_point {
     g_point point;
-}
+};
 
 using map_rd_points = std::map<rd_point_ufi, g_point>;
 
@@ -44,4 +44,4 @@ struct pt_stop_segment {
     pt_stop_id to_stop_id;
     double distance;
     double duration;
-}
+};
